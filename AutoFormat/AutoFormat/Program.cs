71482230cc03461eb164a3c6b9546da37de425e3
Samuel Palmér <palmer.samuel@elev.ga.ntig.se>
2021-12-01 19:36:28 +0100

    Commit

---
+++ AutoFormat/AutoFormat/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+Console.WriteLine("Hello World!");
+  Console.WriteLine("press any key to continue");
+      Console.ReadLine();
+
+  if(  1   == 1)   {
+      Console.Clear();
+  }
---
+++ GolfCoding/GolfCoding/Program.cs
@@ -0,0 +1,18 @@
+﻿using S = System.Console;
+
+S.WriteLine("start");
+S.WriteLine("end");
+S.ReadLine();
+
+
+
+//HUR MAN NORMALT SKRIVER DET!
+/*
+
+using system;
+
+Console.WriteLine("start");
+Console.WriteLine("end");
+Console.ReadLine();
+
+*/
